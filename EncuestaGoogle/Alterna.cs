dfb18194f479ae7547ed373334a1de0aa7dc6674
vmarrerop <77476818+vmarrerop@users.noreply.github.com>
2023-03-19 19:19:48 -0500

    NpsEncuestaDemo

---
+++ EncuestaGoogle/Alterna.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncuestaGoogle
+{
+    internal class Alterna
+    {
+        /*
+        static async Task Main()
+        {
+            // Construir la URL de la solicitud
+            var url = "https://language.googleapis.com/v1/documents:analyzeEntitySentiment";
+
+            // Agregar el token de acceso
+            var token = "AIzaSyAAldH3Yq4U2CVA-0HkUoFYmnTZTDtcoh4";
+
+            // Crear el cliente HttpClient
+            var client = new HttpClient();
+
+            // Agregar el encabezado Authorization
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+            // Crear el cuerpo de la solicitud en formato JSON
+            var requestContent = new
+            {
+                document = new
+                {
+                    type = "PLAIN_TEXT",
+                    content = "I love R&B music. Marvin Gaye is the best. 'What's Going On' is one of my favorite songs. It was so sad when Marvin Gaye died."
+                },
+                encodingType = "UTF8"
+            };
+            var json = JsonConvert.SerializeObject(requestContent);
+            var requestBody = new StringContent(json, Encoding.UTF8, "application/json");
+
+            // Realizar la solicitud HTTP POST
+            var response = await client.PostAsync(url, requestBody);
+
+            // Leer la respuesta de la solicitud
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            // Mostrar la respuesta en la consola
+            Console.WriteLine(responseContent);
+        }
+
+        */
+    }
+}
---
+++ EncuestaGoogle/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+     
+        static async Task Main()
+{
+    // Construir la URL de la solicitud
+    var url = "https://language.googleapis.com/v1/documents:analyzeEntitySentiment";
+
+    // Agregar la clave de API
+    var apiKey = "AIzaSyCVlL4qu69wBGg2kgd2cRo8AMejT5ECuMg";
+
+    // Crear el cliente HttpClient
+    var client = new HttpClient();
+
+    // Agragando la clave Api a la URL
+    url += $"?key=AIzaSyCVlL4qu69wBGg2kgd2cRo8AMejT5ECuMg";
+
+    // Creacion del cuerpo de la solicitud en formato JSON
+    var requestContent = new
+    {
+        document = new
+        {
+            type = "PLAIN_TEXT",
+            content = "Me encanto el servicio de telemedicina."
+        },
+        encodingType = "UTF8"
+    };
+    var json = JsonConvert.SerializeObject(requestContent);
+    var requestBody = new StringContent(json, Encoding.UTF8, "application/json");
+
+    // Realizar la solicitud HTTP POST
+    var response = await client.PostAsync(url, requestBody);
+
+    // Leer la respuesta de la solicitud
+    var responseContent = await response.Content.ReadAsStringAsync();
+
+    // Mostrar la respuesta en la consola
+    Console.WriteLine(responseContent);
+}
+    }
+}
+
+
